d80212c86e30c70c28ac0720ee8c74d659323e2f
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2023-11-27 13:00:20 +0100

    Update Ex5.cs

--- UF2/Ex5.cs
+++ UF2/Ex5.cs
@@ -1,19 +1,21 @@
-﻿using System;
+using System;
 
 namespace Metodes
 {
-    public class Pr5
+    public class Pr4
     {
         public static void Main()
         {
             int num;
             const string MsgInputNum = "Introdueix un número: ";
+            const string MsgCorrect = "És positiu i menor que 100";
+            const string MsgIncorrect = "No és positiu i menor que 100";
 
             Console.WriteLine(MsgInputNum);
             num = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
 
-            Console.WriteLine(Comprovar(num) ? "És positiu i menor que 100" : "No és positiu i menor que 100");
+            Console.WriteLine(Comprovar(num) ? MsgCorrect : MsgIncorrect);
         }
 
         public static bool Comprovar(int num)
