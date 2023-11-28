df0a8493f36a6e46207838b8f302d8b2db390c5c
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2023-11-28 12:31:05 +0100

    Update Ex9.cs

--- UF2/Ex9.cs
+++ UF2/Ex9.cs
@@ -6,42 +6,40 @@ namespace Metodes
     {
         public static void Main()
         {
-            int num;
+            int num, bigger, max, min;
             const string MsgInputNum = "Introdueix un número: ";
+            const string MsgInputBigger = "Quin número ha de ser més gran?  ";
+            const string MsgInputMax = "Introdueix el màxim: ";
+            const string MsgInputMin = "Introdueix el mínim: ";
 
             Console.Write(MsgInputNum);
             num = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
-            Smaller(num);
-            Console.WriteLine();
-            Range(num);
-        }
-
-        public static void Smaller(int num)
-        {
-            int bigger;
-            const string MsgInputBigger = "Quin número ha de ser més gran?  ";
-            const string MsgIsSmaller = "{0} és més petit que {1}.";
-            const string MsgIsNotSmaller = "{0} no és més petit que {1}.";
 
             Console.Write(MsgInputBigger);
             bigger = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine((num < bigger ? MsgIsSmaller : MsgIsNotSmaller), num, bigger);
-        }
-
-        public static void Range(int num)
-        {
-            int max, min;
-            const string MsgInputMax = "Introdueix el màxim: ";
-            const string MsgInputMin = "Introdueix el mínim: ";
-            const string MsgInRange = "El número {0} es troba entre {1} i {2}.";
-            const string MsgNotInRange = "El número {0} no es troba entre {1} i {2}.";
+            Smaller(num, bigger);
+            Console.WriteLine();
 
             Console.Write(MsgInputMin);
             min = Convert.ToInt32(Console.ReadLine());
             Console.Write(MsgInputMax);
             max = Convert.ToInt32(Console.ReadLine());
+            Range(num, min, max);
+        }
+
+        public static void Smaller(int num, int bigger)
+        {
+            const string MsgIsSmaller = "{0} és més petit que {1}.";
+            const string MsgIsNotSmaller = "{0} no és més petit que {1}.";
+
+            Console.WriteLine((num < bigger ? MsgIsSmaller : MsgIsNotSmaller), num, bigger);
+        }
+
+        public static void Range(int num, int min, int max)
+        {
+            const string MsgInRange = "El número {0} es troba entre {1} i {2}.";
+            const string MsgNotInRange = "El número {0} no es troba entre {1} i {2}.";
 
             Console.WriteLine((((num > min) && (num < max)) ? MsgInRange : MsgNotInRange), num, min, max);
         }
