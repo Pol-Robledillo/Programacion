0a8c16eaddd26b5a833786e587588ff4dff05b65
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2023-11-28 15:16:09 +0400

    Add files via upload

---
+++ UF2/Ex10.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Metodes
+{
+    class Pr8
+    {
+        public static void Main()
+        {
+            int searchNum;
+            const int Numbers = 10, Min = 0, Max = 200;
+            const string MsgInputSearch = "Introdueix quin número vols buscar: ";
+            const string MsgNumberFound = "El número {0} es troba a la llista.";
+            const string MsgNumberNotFound = "El número {0} no es troba a la llista.";
+            int[] randomNumbers = new int[Numbers];
+            Random randomNumber = new Random();
+
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+                randomNumbers[i] = randomNumber.Next(Min, Max);
+            }
+
+            Console.Write(MsgInputSearch);
+            searchNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            
+            Console.WriteLine((InArray(searchNum, randomNumbers) ? MsgNumberFound : MsgNumberNotFound), searchNum);
+        }
+
+        public static bool InArray(int searchNum, int[] list)
+        {
+            int search = 0;
+            bool found = false;
+
+            while ((search < list.Length) && (!found))
+            {
+                if (list[search] == searchNum)
+                {
+                    found = true;
+                }
+                search++;
+            }
+            return found;
+        }
+    }
+}
---
+++ UF2/Ex11.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Metodes
+{
+    class Pr8
+    {
+        public static void Main()
+        {
+            int num;
+            bool invalidNum = false;
+            const string MsgInput = "Introdueix un número: ";
+            const string MsgInvalidNum = "El número no pot ser negatiu.";
+            const string MsgOutput = "El factorial de {0} és {1}.";
+            do
+            {
+                if (invalidNum)
+                {
+                    Console.WriteLine(MsgInvalidNum);
+                    invalidNum = false;
+                }
+                Console.Write(MsgInput);
+                num = Convert.ToInt32(Console.ReadLine());
+                invalidNum = true;
+            } while (num < 0);
+
+            Console.WriteLine(MsgOutput, num, Factorial(num));
+        }
+
+        public static int Factorial(int num)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
