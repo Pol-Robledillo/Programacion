d29a5c7af5eb0964fe1a161dd20d399f3b76248c
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2023-12-05 12:20:29 +0100

    Add files via upload

---
+++ UF2/Ex15.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Actividades
+{
+    public class Ex15
+    {
+        public static void Main()
+        {
+            int year;
+            const string MsgInputNumber = "Introdueix l'any: ";
+            const string MsgLeapYear = "Aquest any és de traspas.";
+            const string MsgNotLeapYear = "Aquest any no és de traspas.";
+
+            Console.Write(MsgInputNumber);
+            year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            if (ValidateYear(year))
+            {
+                Console.WriteLine(MsgLeapYear);
+            } else
+            {
+                Console.WriteLine(MsgNotLeapYear);
+            }
+        }
+        public static bool ValidateYear(int year)
+        {
+            if ((year % 4) == 0)
+            {
+                if ((year % 100) == 0)
+                {
+                    if ((year % 400) == 0)
+                    {
+                        return true;
+                    } else 
+                    {
+                        return false;
+                    }
+                } else 
+                {
+                    return true;
+                }
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
