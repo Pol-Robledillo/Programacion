8e166b682ba03d8342bf14ad1c26808141bb8a04
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2023-12-05 13:50:54 +0100

    Update Ex9.cs

--- UF2/Ex9.cs
+++ UF2/Ex9.cs
@@ -7,14 +7,15 @@ namespace Metodes
         public static void Main()
         {
             int num, bigger, max, min;
+            string validation;
             const string MsgInputNum = "Introdueix un número: ";
             const string MsgInputBigger = "Quin número ha de ser més gran?  ";
             const string MsgInputMax = "Introdueix el màxim: ";
             const string MsgInputMin = "Introdueix el mínim: ";
-            const string MsgIsSmaller = "{0} és més petit que {1}.";
-            const string MsgIsNotSmaller = "{0} no és més petit que {1}.";
-            const string MsgInRange = "El número {0} es troba entre {1} i {2}.";
-            const string MsgNotInRange = "El número {0} no es troba entre {1} i {2}.";
+            const string MsgIsSmaller = "El número a validar és més petit que el número indicat.";
+            const string MsgIsNotSmaller = "El número a validar no és més petit que el número indicat.";
+            const string MsgInRange = "El número es troba en el rang indicat.";
+            const string MsgNotInRange = "El número no es troba en el rang indicat.";
 
             Console.Write(MsgInputNum);
             num = Convert.ToInt32(Console.ReadLine());
@@ -22,27 +23,16 @@ namespace Metodes
 
             Console.Write(MsgInputBigger);
             bigger = Convert.ToInt32(Console.ReadLine());
-            if (Validate(num, bigger))
-            {
-                Console.WriteLine(MsgIsSmaller);
-            } else
-            {
-                Console.WriteLine(MsgIsNotSmaller);
-            }
+            validation = Validate(num, bigger) ? MsgIsSmaller : MsgIsNotSmaller;
+            Console.WriteLine(validation);
             Console.WriteLine();
 
             Console.Write(MsgInputMin);
             min = Convert.ToInt32(Console.ReadLine());
             Console.Write(MsgInputMax);
             max = Convert.ToInt32(Console.ReadLine());
-            if (Validate(num, min, max))
-            {
-                Console.WriteLine(MsgInRange);
-            }
-            else
-            {
-                Console.WriteLine(MsgNotInRange);
-            }
+            validation = (Validate(num, min, max) ? MsgInRange : MsgNotInRange);
+            Console.WriteLine(validation);
         }
 
         public static bool Validate(int num, int bigger)
