ac080d14216157c8f7d0ef6f6e3429e5f0bee6a4
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2023-12-13 14:00:12 +0100

    Add files via upload

---
+++ UF2/Ex17.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Metodes
+{
+    class Pr17
+    {
+        public static void Main()
+        {
+            double temp;
+            const string MsgInputCelsius = "Introdueix la temperatura en graus Celsius: ";
+            const string MsgOutputFahrenheit = "La temperatura en graus Fahrenheit és {0}.";
+
+            Console.Write(MsgInputCelsius);
+            temp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine(MsgOutputFahrenheit, FahrenheitConversion(temp));
+        }
+
+        public static double FahrenheitConversion(double temp)
+        {
+            return (((temp * 9) / 5) + 32);
+        }
+    }
+}
---
+++ UF2/Ex18.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Metodes
+{
+    class Pr18
+    {
+        public static void Main()
+        {
+            bool error = false;
+            double time;
+            const string MsgInputMinutes = "Introdueix el temps en minuts: ";
+            const string MsgError = "El temps no pot ser negatiu.";
+            const string MsgOutputSeconds = "{0} minuts són {1} segons.";
+
+            do
+            {
+                Console.Write(MsgInputMinutes);
+                time = Convert.ToDouble(Console.ReadLine());
+                if (!ValidateTime(time)) {
+                    error = true;
+                    Console.WriteLine(MsgError);
+                }
+                Console.WriteLine();
+            } while (error);
+
+            Console.WriteLine(MsgOutputSeconds, time, MinutesToSeconds(time));
+        }
+
+        public static bool ValidateTime(double time)
+        {
+            return time > 0 ? true : false;
+        }
+
+        public static double MinutesToSeconds(double time)
+        {
+            return (time * 60);
+        }
+    }
+}
