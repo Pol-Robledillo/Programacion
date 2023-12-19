7cac54e3f0db179bd8aec9a3db951dd1ddd60c02
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2023-12-19 13:18:33 +0100

    Add files via upload

---
+++ UF2/Ex19.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Metodes
+{
+    class Pr19
+    {
+        public static void Main()
+        {
+            int firstNum, secondNum;
+            const string MsgInputFirstNum = "Introdueix el primer número: ";
+            const string MsgInputSecondNum = "Introdueix el segon número: ";
+            const string MsgOutputMax = "El màxim és el {0}.";
+            const string MsgOutputMin = "El mínim és el {0}.";
+
+            Console.Write(MsgInputFirstNum);
+            firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(MsgInputSecondNum);
+            secondNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(MsgOutputMax, Math.Max(firstNum, secondNum));
+            Console.WriteLine(MsgOutputMin, Math.Min(firstNum, secondNum));
+        }
+    }
+}
---
+++ UF2/Ex20.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Metodes
+{
+    class Pr20
+    {
+        public static void Main()
+        {
+            double num, decimals;
+            const string MsgInputNumber = "Introdueix un número decimal: ";
+            const string MsgOutputNumber = "La part entera del número és {0} i la part decimal és {1}.";
+
+            Console.Write(MsgInputNumber);
+            num = Convert.ToDouble(Console.ReadLine());
+
+            decimals = GetDecimals(num);
+
+            Console.WriteLine(MsgOutputNumber, (Math.Round(num, MidpointRounding.ToZero)), decimals);
+        }
+        public static double GetDecimals(double num)
+        {
+            return num - (Math.Round(num, MidpointRounding.ToZero));
+        }
+    }
+}
