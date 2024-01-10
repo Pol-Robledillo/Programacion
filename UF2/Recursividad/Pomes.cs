cecfd81800f3bf7e07dd242794424521f205dcec
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-01-10 12:30:34 +0100

    Add files via upload

---
+++ UF2/Recursividad/Pomes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.Versioning;
+
+namespace Metodes
+{
+    class Pometes
+    {
+        public static void Main()
+        {
+            int pometes;
+            const string MsgInputFirst = "Introdueix el número de pomes: ";
+
+            Console.Write(MsgInputFirst);
+            pometes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            CountPometes(pometes);
+        }
+        public static void CountPometes(int pometes)
+        {
+            const string MsgLyrics = "{0} {1} té el pomer," +
+                "\r\nde {0} una, de {0} una," +
+                "\r\n{0} {1} té el pomer," +
+                "\r\nde {0} una en caigué." +
+                "\r\n" +
+                "\r\nSi mireu el vent d'on vé" +
+                "\r\nveureu el pomer com dansa," +
+                "\r\nsi mireu el vent d'on vé" +
+                "\r\nveureu com dansa el pomer.\n";
+            if (pometes == 1)
+            {
+                Console.WriteLine(MsgLyrics, pometes, "pometa");
+            } else
+            {
+                Console.WriteLine(MsgLyrics, pometes, "pometes");
+                CountPometes(pometes - 1);
+            }
+        }
+    }
+}
