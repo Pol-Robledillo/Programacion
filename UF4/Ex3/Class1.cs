8ff225ec012264e4203f18098c2a531807050d75
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-01-29 20:54:06 +0100

    Add files via upload

---
+++ UF4/Ex3/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosOOP
+{
+    public class Bicycle
+    {
+        private int price;
+        private string brand, color;
+
+        public int Price { get { return price; } set { price = value; } }
+        public string Brand { get { return brand; } set { brand = value; } }
+        public string Color { get { return color; } set { color = value; } }
+
+        public Bicycle(int price, string brand, string color)
+        {
+            this.price = price;
+            this.brand = brand;
+            this.color = color;
+        }
+    }
+}
---
+++ UF4/Ex3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EjerciciosOOP;
+namespace OOP
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Bicycle mountainBike = new Bicycle(100, "Noseque", "Red");
+            Bicycle roadBike = new Bicycle(200, "Noseque2", "Blue");
+            Console.WriteLine("Mountain bike price: " + mountainBike.Price);
+            Console.WriteLine("Road bike price: " + roadBike.Price);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Mountain bike brand: " + mountainBike.Brand);
+            Console.WriteLine("Road bike brand: " + roadBike.Brand);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Mountain bike color: " + mountainBike.Color);
+            Console.WriteLine("Road bike color: " + roadBike.Color);
+        }
+    }
+}
