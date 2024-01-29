aa05a33c48c02bf6a3d75827d647c15cc042f702
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-01-29 21:05:54 +0100

    Add files via upload

---
+++ UF4/Ex4/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosOOP
+{
+    public class Cat
+    {
+        private int age;
+        private string name, breed, favFood;
+
+        public int Age { get { return age; } set { age = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public string Breed { get { return breed; } set { breed = value; } }
+        public string FavFood { get { return favFood; } set { favFood = value; } }
+
+        public Cat(int age, string name, string breed, string favFood)
+        {
+            this.age = age;
+            this.name = name;
+            this.breed = breed;
+            this.favFood = favFood;
+        }
+        public void Meow(string name)
+        {
+            Console.WriteLine($"{name} is meowing");
+        }
+        public void Play(string name)
+        {
+            Console.WriteLine($"{name} is playing");
+        }
+        public void Wash(string name)
+        {
+            Console.WriteLine($"{name} is washing himself");
+        }
+        public void Eat(string name, string favFood)
+        {
+            Console.WriteLine($"{name} is eating {favFood}");
+        }
+        public void Hunt(string name)
+        {
+            Console.WriteLine($"{name} is hunting");
+        }
+    }
+}
---
+++ UF4/Ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EjerciciosOOP;
+namespace OOP
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Cat cat1 = new Cat(4, "Francisco", "Persian", "Sardines");
+            Cat cat2 = new Cat(6, "Adolf", "German Rex", "Milk");
+            Cat cat3 = new Cat(5, "Benito", "Sphynx", "Tuna");
+            Cat cat4 = new Cat(2, "Kim", "Li Hua", "Salmon");
+
+            cat1.Meow(cat1.Name);
+            cat2.Play(cat2.Name);
+            cat3.Wash(cat3.Name);
+            cat4.Eat(cat4.Name, cat4.FavFood);
+            cat1.Hunt(cat1.Name);
+        }
+    }
+}
