34ddc537266a810d9203ab5cbe94c55be3347984
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-01-29 13:46:42 +0100

    Add files via upload

---
+++ UF4/Ex1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosOOP
+{
+    public class Person
+    {
+        private string name, surname;
+        private int age;
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetSurname(string surname)
+        {
+            this.surname = surname;
+        }
+        public string GetSurname()
+        {
+            return surname;
+        }
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+        public int GetAge()
+        {
+            return age;
+        }
+    }
+}
---
+++ UF4/Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EjerciciosOOP;
+namespace OOP
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Person person = new Person();
+            person.SetName("Pol");
+            person.SetSurname("Robledillo");
+            person.SetAge(18);
+            Console.WriteLine("Name: {0}", person.GetName());
+            Console.WriteLine("Surname: {0}", person.GetSurname());
+            Console.WriteLine("Age: {0}", person.GetAge());
+        }
+    }
+}
