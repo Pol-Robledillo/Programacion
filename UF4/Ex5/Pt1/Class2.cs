d6aef09a5de382cb79ac04d1e44626db3c460478
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-02-04 19:50:05 +0100

    Add files via upload

---
+++ UF4/Ex5/Pt1/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosOOP
+{
+    public class NumCard : ADeck
+    {
+        public int Number { get; set; }
+        public NumCard(int number, string color, string type)
+        {
+            Number = number;
+            Color = color;
+            Type = type;
+        }
+    }
+}
---
+++ UF4/Ex5/Pt1/Class3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosOOP
+{
+    public class EffectCard : ADeck
+    {
+        public string Effect { get; set; }
+        public EffectCard(string effect, string color, string type)
+        {
+            Effect = effect;
+            Color = color;
+            Type = type;
+        }
+    }
+}
