883e3649e2c1298951cc36147ebb65993b0b9df6
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-02-20 12:18:49 +0100

    Add files via upload

---
+++ UF4/Ex17/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosOOP
+{
+    public class Car : IVehicle
+    {
+        public int Fuel { get; set; }
+        public Car(int fuel)
+        {
+            Fuel = fuel;
+        }
+        public void Drive()
+        {
+            if (Fuel > 0)
+            {
+                Console.WriteLine("Driving");
+            }
+        }
+        public bool Refuel(int Amount)
+        {
+            Fuel += Amount;
+            return true;
+        }
+    }
+}
---
+++ UF4/Ex17/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace EjerciciosOOP
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Car myCar = new Car(0);
+            myCar.Drive();
+            myCar.Refuel(100);
+            myCar.Drive();
+        }
+    }
+}
