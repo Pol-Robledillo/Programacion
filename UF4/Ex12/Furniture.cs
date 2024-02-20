2dc04f50b1741dbf86a87f0e28f778c4f161b682
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-02-20 11:53:11 +0100

    Add files via upload

---
+++ UF4/Ex12/Furniture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosOOP
+{
+    internal class Furniture
+    {
+        public float Weight { get; set; }
+        public double Price { get; set; }
+        public bool Set { get; set; }
+        public string Color { get; set; }
+        public Furniture(float weight, double price, bool set, string color)
+        {
+            Weight = weight;
+            Price = price;
+            Set = set;
+            Color = color;
+        }
+    }
+}
