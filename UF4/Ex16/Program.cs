54c04892eaec6e47a301c5e2a7bacc5835be5c36
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-02-20 12:07:38 +0100

    Add files via upload

---
+++ UF4/Ex16/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace OOP
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            PaymentCard card = new PaymentCard(50);
+            Console.WriteLine(card);
+            card.Balance = 150;
+            Console.WriteLine(card);
+        }
+    }
+}
