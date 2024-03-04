85332e9f37da53bef0eeda1f97ddf1f06bbb3a10
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-04 13:47:18 +0100

    Add files via upload

---
+++ UF5/Ex1/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesUF5
+{
+    public class Book : IComparable<Book>
+    {
+        public static int CurrentId = 1;
+        const string DefaultTitle = "Unknown";
+        const int DefaultNumPages = 0;
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public int NumPages { get; set; }
+        public Book(string title, int numPages)
+        {
+            Id = CurrentId;
+            Title = title;
+            NumPages = numPages;
+            CurrentId++;
+        }
+        public Book()
+        {
+            Id = CurrentId;
+            Title = DefaultTitle;
+            NumPages = DefaultNumPages;
+            CurrentId++;
+        }
+        public override string ToString()
+        {
+            return "Id: " + Id + " | Title: " + Title + " | NumPages: " + NumPages;
+        }
+        public int CompareTo(Book? other)
+        {
+            return this.NumPages.CompareTo(other?.NumPages);
+        }
+    }
+}
---
+++ UF5/Ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+namespace ActividadesUF5
+{
+    public class Ex1
+    {
+        public static void Main()
+        {
+            List<Book> books = new List<Book>();
+            books.Add(new Book("El Hobbit", 345));
+            books.Add(new Book("El Quijote", 500));
+            books.Add(new Book());
+            books.Add(new Book("Los Juegos del Hambre", 267));
+            books.Add(new Book("El Señor de los Anillos", 433));
+            books.Sort();
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+            Console.WriteLine("------------------------------------------------");
+            books.Sort(new BookComparer());
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
