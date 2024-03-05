0f5a999e6412a20024e1776431854ff00eb66fac
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-05 10:33:19 +0100

    Add files via upload

---
+++ UF5/Ex6/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesUF5
+{
+    public class Employee
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Employee (string name, string surname, DateTime birthDate)
+        {
+            Name = name;
+            Surname = surname;
+            BirthDate = birthDate;
+        }
+        public int GetAge()
+        {
+            DateTime actual = DateTime.Now;
+            int age = actual.Year - BirthDate.Year;
+            if (actual.Month < BirthDate.Month || (actual.Month == BirthDate.Month && actual.Day < BirthDate.Day))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
---
+++ UF5/Ex6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex4
+    {
+        public static void Main()
+        {
+            Employee employee = new Employee("Pol", "Robledillo", new DateTime(2005, 5, 12));
+            Console.WriteLine(employee.GetAge());
+        }
+    }
+}
