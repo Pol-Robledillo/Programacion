77fb34a9d7813c0d06b6c4e0b99f422c3bc59707
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-05 12:03:47 +0100

    Update Program.cs

--- UF5/Ex7/Program.cs
+++ UF5/Ex7/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 namespace ActividadesUF5
 {
     public class Ex4
@@ -17,11 +17,11 @@ namespace ActividadesUF5
                 Console.WriteLine(emp.ToString());
             }
             List<SalesEmployee> salesEmployees = new List<SalesEmployee>();
-            salesEmployees.Add(new SalesEmployee("SE1", "Peter", "Parker", new DateTime(1962, 1, 1), new DateTime(2016, 1, 1), 2000, 15, 0.1f));
-            salesEmployees.Add(new SalesEmployee("SE2", "Clark", "Kent", new DateTime(1938, 1, 1), new DateTime(2015, 1, 1), 2000, 15, 0.1f));
-            salesEmployees.Add(new SalesEmployee("SE3", "Bruce", "Wayne", new DateTime(1939, 1, 1), new DateTime(2008, 1, 1), 2000, 15, 0.1f));
-            salesEmployees.Add(new SalesEmployee("SE4", "Tony", "Stark", new DateTime(1963, 1, 1), new DateTime(2008, 1, 1), 2000, 15, 0.1f));
-            salesEmployees.Add(new SalesEmployee("SE5", "Steve", "Rogers", new DateTime(1941, 1, 1), new DateTime(2009, 1, 1), 2000, 15, 0.1f));
+            salesEmployees.Add(new SalesEmployee("SE1", "Peter", "Parker", new DateTime(1962, 1, 1), new DateTime(2016, 1, 1), 2000, 15, 134.1f));
+            salesEmployees.Add(new SalesEmployee("SE2", "Clark", "Kent", new DateTime(1938, 1, 1), new DateTime(2015, 1, 1), 2000, 15, 426.1f));
+            salesEmployees.Add(new SalesEmployee("SE3", "Bruce", "Wayne", new DateTime(1939, 1, 1), new DateTime(2008, 1, 1), 2000, 15, 500.1f));
+            salesEmployees.Add(new SalesEmployee("SE4", "Tony", "Stark", new DateTime(1963, 1, 1), new DateTime(2008, 1, 1), 2000, 15, 1345.6f));
+            salesEmployees.Add(new SalesEmployee("SE5", "Steve", "Rogers", new DateTime(1941, 1, 1), new DateTime(2009, 1, 1), 2000, 15, 1000f));
             salesEmployees.Sort();
             foreach (SalesEmployee emp in salesEmployees)
             {
