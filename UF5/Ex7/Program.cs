e6d04377002bc1d864c3734eae1f865f614be099
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-05 11:56:59 +0100

    Add files via upload

---
+++ UF5/Ex7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex4
+    {
+        public static void Main()
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee("E1", "Pol", "Robledillo", new DateTime(2005, 5, 12), new DateTime(2020, 5, 12), 2000, 14));
+            employees.Add(new Employee("E2", "Pau", "Robledillo", new DateTime(2004, 4, 12), new DateTime(2018, 5, 12), 2000, 14));
+            employees.Add(new Employee("E3", "Pep", "Robledillo", new DateTime(2003, 3, 12), new DateTime(2016, 5, 12), 2000, 14));
+            employees.Add(new Employee("E4", "Pere", "Robledillo", new DateTime(2002, 2, 12), new DateTime(2014, 5, 12), 2000, 14));
+            employees.Add(new Employee("E5", "Pablo", "Robledillo", new DateTime(2001, 1, 12), new DateTime(2012, 5, 12), 2000, 14));
+            employees.Sort();
+            foreach (Employee emp in employees)
+            {
+                Console.WriteLine(emp.ToString());
+            }
+            List<SalesEmployee> salesEmployees = new List<SalesEmployee>();
+            salesEmployees.Add(new SalesEmployee("SE1", "Peter", "Parker", new DateTime(1962, 1, 1), new DateTime(2016, 1, 1), 2000, 15, 0.1f));
+            salesEmployees.Add(new SalesEmployee("SE2", "Clark", "Kent", new DateTime(1938, 1, 1), new DateTime(2015, 1, 1), 2000, 15, 0.1f));
+            salesEmployees.Add(new SalesEmployee("SE3", "Bruce", "Wayne", new DateTime(1939, 1, 1), new DateTime(2008, 1, 1), 2000, 15, 0.1f));
+            salesEmployees.Add(new SalesEmployee("SE4", "Tony", "Stark", new DateTime(1963, 1, 1), new DateTime(2008, 1, 1), 2000, 15, 0.1f));
+            salesEmployees.Add(new SalesEmployee("SE5", "Steve", "Rogers", new DateTime(1941, 1, 1), new DateTime(2009, 1, 1), 2000, 15, 0.1f));
+            salesEmployees.Sort();
+            foreach (SalesEmployee emp in salesEmployees)
+            {
+                Console.WriteLine(emp.ToString());
+            }
+        }
+    }
+}
