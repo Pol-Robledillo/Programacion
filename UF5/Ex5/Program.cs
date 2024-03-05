2dc4627d2d104ec3227bc4fe8a1a032c66a76a1d
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-05 10:23:19 +0100

    Add files via upload

---
+++ UF5/Ex5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex4
+    {
+        public static void Main()
+        {
+            try
+            {
+                DateTime actual = DateTime.Now;
+
+                Console.WriteLine(actual.DayOfWeek.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
