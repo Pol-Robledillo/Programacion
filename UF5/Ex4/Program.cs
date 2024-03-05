eb63f6186efb2df234876636e0a07efbd71f6393
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-05 10:20:50 +0100

    Add files via upload

---
+++ UF5/Ex4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex4
+    {
+        public static void Main()
+        {
+            try
+            {
+                int day, month, year;
+                Console.WriteLine("Introduce un dia: ");
+                day = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Introduce un mes: ");
+                month = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Introduce un año: ");
+                year = Convert.ToInt32(Console.ReadLine());
+                DateTime firstDate = new DateTime(year, month, day);
+
+                Console.WriteLine("Introduce un dia: ");
+                day = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Introduce un mes: ");
+                month = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Introduce un año: ");
+                year = Convert.ToInt32(Console.ReadLine());
+                DateTime secondDate = new DateTime(year, month, day);
+
+                Console.WriteLine(firstDate < secondDate ? "La segunda fecha es posterior a la primera" : "La segunda fecha no es posterior la primera");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
