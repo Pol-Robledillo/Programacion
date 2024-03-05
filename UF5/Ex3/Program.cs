e8c320a746857ebdb37c33aedea24784d8c96420
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-05 10:16:19 +0100

    Add files via upload

---
+++ UF5/Ex3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Veterinario
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                int day, month, year;
+                Console.WriteLine("Introduce un dia: ");
+                day = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Introduce un mes: ");
+                month = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Introduce un año: ");
+                year = Convert.ToInt32(Console.ReadLine());
+                DateTime date = new DateTime(year, month, day);
+
+                if (date > DateTime.Now)
+                {
+                    TimeSpan difference = date - DateTime.Now;
+                    Console.WriteLine("Falta " + difference.Days + " para la fecha indicada");
+                }
+                else
+                {
+                    TimeSpan difference = DateTime.Now - date;
+                    Console.WriteLine("Han pasado " + difference.Days + " desde la fecha indicada");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
