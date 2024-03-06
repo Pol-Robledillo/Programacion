5bcd497ca35483297341f4c90357398e8fde89ef
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-06 11:46:03 +0100

    Add files via upload

---
+++ UF5/Ex12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+namespace ActividadesUF5
+{
+    public class Ex11
+    {
+        public static void Main()
+        {
+            List<int> nums = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            nums.RemoveAll(x => x > 6);
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
