6c1329ffea01aa75fb8051a5ad28058b1af94ad8
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-06 11:31:20 +0100

    Update Program.cs

--- UF5/Ex8/Program.cs
+++ UF5/Ex8/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 namespace ActividadesUF5
 {
@@ -6,18 +6,8 @@ namespace ActividadesUF5
     {
         public static void Main()
         {
-            int suma = 0;
-            List<int> list = new List<int>();
-            list.Add(1);
-            list.Add(2);
-            list.Add(3);
-            list.Add(4);
-            list.Add(5);
-            foreach (int i in list)
-            {
-                suma += i;
-            }
-            Console.WriteLine("La suma de los elementos de la lista es: " + suma);
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("La suma de los elementos de la lista es: " + list.Sum());
         }
     }
 }
