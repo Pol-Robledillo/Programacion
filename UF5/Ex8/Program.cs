0354d176a6234a91e445be92a63d8c3d8bac50e0
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-06 11:27:56 +0100

    Add files via upload

---
+++ UF5/Ex8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+namespace ActividadesUF5
+{
+    public class Ex8
+    {
+        public static void Main()
+        {
+            int suma = 0;
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            foreach (int i in list)
+            {
+                suma += i;
+            }
+            Console.WriteLine("La suma de los elementos de la lista es: " + suma);
+        }
+    }
+}
