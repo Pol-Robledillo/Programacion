0cfa27c7d2da74c8f519c6d9cbe640e95d8401fe
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-06 11:34:42 +0100

    Add files via upload

---
+++ UF5/Ex9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+namespace ActividadesUF5
+{
+    public class Ex9
+    {
+        public static void Main()
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach (int i in list.FindAll(x => x % 2 == 0))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
