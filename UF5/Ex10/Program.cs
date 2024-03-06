efea7af7744f74d6b92d83078281358b4fc6cc81
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-06 11:36:56 +0100

    Add files via upload

---
+++ UF5/Ex10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+namespace ActividadesUF5
+{
+    public class Ex10
+    {
+        public static void Main()
+        {
+            string[] words = {"hola", "adios", "que tal", "hasta luego"};
+            List<string> list = new List<string>(words);
+            foreach (string word in list)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
