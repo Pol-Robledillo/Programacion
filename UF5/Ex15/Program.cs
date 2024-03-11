acd4920f08c11a50b2ea162b85f7c40d8b4b81b6
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-11 11:42:59 +0100

    Add files via upload

---
+++ UF5/Ex15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+namespace ActividadesUF5
+{
+    public class Ex15
+    {
+        public static void Main()
+        {
+            string email = "polrobledilloortega@gmail.com";
+            Regex regex = new Regex(@"\S+@\S+\.[a-z]");
+            if (regex.IsMatch(email))
+            {
+                Console.WriteLine("El email " + email + " tiene un formato válido");
+            }
+            else
+            {
+                Console.WriteLine("El email " + email + " no tiene un formato válido");
+            }
+        }
+    }
+}
