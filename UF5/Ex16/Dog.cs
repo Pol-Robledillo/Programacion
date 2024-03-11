347f3f67ec9f3497fd1de064f9832a7695bfed3d
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-11 12:00:58 +0100

    Add files via upload

---
+++ UF5/Ex16/Dog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesUF5
+{
+    public class Dog
+    {
+        public string? Name { get; set; }
+        public string? Breed { get; set; }
+        public int Age { get; set; }
+        public Dog(string? name, string? breed, int age)
+        {
+            Name = name;
+            Breed = breed;
+            Age = age;
+        }
+        public override String ToString()
+        {
+            StringBuilder dogInfo = new StringBuilder();
+            dogInfo.Append("Name: ");
+            dogInfo.Append(Name);
+            dogInfo.Append(", Breed: ");
+            dogInfo.Append(Breed);
+            dogInfo.Append(", Age: ");
+            dogInfo.Append(Age);
+            return dogInfo.ToString();
+        }
+    }
+}
---
+++ UF5/Ex16/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+namespace ActividadesUF5
+{
+    public class Ex16
+    {
+        public static void Main()
+        {
+            Dog dog = new Dog("Firulais", "Bulldog", 3);
+            Console.WriteLine(dog.ToString());
+        }
+    }
+}
