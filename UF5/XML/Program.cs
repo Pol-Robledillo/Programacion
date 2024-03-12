598eceebf11e00a92d3ecf0bc26a4a10523a5d6b
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-12 13:27:41 +0100

    Add files via upload

---
+++ UF5/XML/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Xml;
+namespace ActividadesUF5
+{
+    public class PruebaXML
+    {
+        public static void Main()
+        {
+            string file = @"..\..\..\files\pokemon.xml";
+            XmlDocument doc = new XmlDocument();
+            doc.Load(file);
+            foreach (XmlNode pokemon in doc.DocumentElement.ChildNodes)
+            {
+                if (pokemon.SelectSingleNode("types").InnerText.Contains("fire"))
+                {
+                    foreach (XmlNode info in pokemon.ChildNodes)
+                    {
+                        Console.WriteLine(info.Name + ": " + info.InnerText);
+                    }
+                }
+            }
+        }
+    }
+}
