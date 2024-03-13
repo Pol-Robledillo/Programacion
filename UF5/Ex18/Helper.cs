2c37058d380d3fee09b62c17cdb17af6a1ef7659
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-13 13:29:04 +0100

    Add files via upload

---
+++ UF5/Ex18/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesUF5
+{
+    public static class Helper
+    {
+        public static int Compare(Book? x, Book? y)
+        {
+            if (x == null || y == null) return 0;
+            if (x == null) return -1;
+            if (y == null) return 1;
+            return -x.Year.CompareTo(y.Year);
+        }
+    }
+}
---
+++ UF5/Ex18/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ActividadesUF5
+{
+    public class Ex18
+    {
+        public delegate int CompareBooks(Book b1, Book b2);
+        public static void Main()
+        {
+            CompareBooks CustomCompare = Helper.Compare;
+            List<Book> books =
+            [
+                new Book("The Hobbit", "J.R.R. Tolkien", 1937),
+                new Book("The Fellowship of the Ring", "J.R.R. Tolkien", 1954),
+                new Book("The Two Towers", "J.R.R. Tolkien", 1954),
+                new Book("The Return of the King", "J.R.R. Tolkien", 1955),
+            ];
+            books.Sort((b1, b2) => CustomCompare(b1, b2));
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
