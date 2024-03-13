b1409945ab64c4937d6642aced7eae2e011b0936
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-13 13:49:19 +0100

    Add files via upload

---
+++ UF5/Ex21/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesUF5
+{
+    public static class Helper
+    {
+        public static double CalcAvg(double[] nums)
+        {
+            return nums.Sum() / nums.Length;
+        }
+    }
+}
---
+++ UF5/Ex21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex19
+    {
+        public delegate double MyAvg(double[] nums);
+        public static void Main()
+        {
+            double[] nums = [ 4, 77, 3, 12, 56 ];
+            MyAvg CalcRaiz = Helper.CalcAvg;
+            double result = CalcRaiz(nums);
+            Console.WriteLine(result);
+        }
+    }
+}
