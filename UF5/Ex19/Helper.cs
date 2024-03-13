371d339292dc1111f153dba6983cfaff5b326989
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-13 13:38:50 +0100

    Add files via upload

---
+++ UF5/Ex19/Helper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesUF5
+{
+    public static class Helper
+    {
+        public static int CalcPotencia(int num, int exp)
+        {
+            if (exp == 0)
+            {
+                return 1;
+            } else if (exp == 1)
+            {
+                return num;
+            } else
+            {
+                return num * CalcPotencia(num, exp - 1);
+            }
+        }
+    }
+}
---
+++ UF5/Ex19/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex19
+    {
+        public delegate int Potencia(int num, int exp);
+        public static void Main()
+        {
+            Potencia CalcPotencia = Helper.CalcPotencia;
+            int result = CalcPotencia(2, 3);
+            Console.WriteLine(result);
+        }
+    }
+}
