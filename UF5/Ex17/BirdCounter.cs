fda948871f907435619656de99e7efd7836d4234
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-13 11:26:35 +0100

    Add files via upload

---
+++ UF5/Ex17/BirdCounter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesUF5
+{
+    public class BirdCounter
+    {
+        public int[]? BirdsPerDay { get; set; }
+        public void LastWeek()
+        {
+            foreach (int day in BirdsPerDay)
+            {
+                Console.Write(day + ", ");
+            }
+            Console.WriteLine();
+        }
+        public int Today()
+        {
+            return BirdsPerDay[BirdsPerDay.Length - 1];
+        }
+        public void IncrementTodaysCount()
+        {
+            BirdsPerDay[BirdsPerDay.Length - 1]++;
+        }
+        public bool HasDayWithoutBirds()
+        {
+            return BirdsPerDay.Contains(0);
+        }
+        public int TotalBirds()
+        {
+            int total = 0;
+            foreach (int day in BirdsPerDay)
+            {
+                total += day;
+            }
+            return total;
+        }
+        public int BusyDays()
+        {
+            int busyDays = 0;
+            foreach (int day in BirdsPerDay)
+            {
+                if (day >= 5)
+                {
+                    busyDays++;
+                }
+            }
+            return busyDays;
+        }
+    }
+}
---
+++ UF5/Ex17/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ActividadesUF5
+{
+    public class Ex17
+    {
+        public static void Main()
+        {
+            BirdCounter birdCounter = new BirdCounter();
+            birdCounter.BirdsPerDay = new int[] { 2, 5, 0, 7, 4, 1 };
+            birdCounter.LastWeek();
+            Console.WriteLine(birdCounter.Today());
+            birdCounter.IncrementTodaysCount();
+            birdCounter.LastWeek();
+            Console.WriteLine(birdCounter.HasDayWithoutBirds());
+            Console.WriteLine(birdCounter.TotalBirds());
+            Console.WriteLine(birdCounter.BusyDays());
+        }
+    }
+}
