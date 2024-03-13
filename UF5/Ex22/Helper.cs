7b27918065dbff2da9968b47725ac68a5618d4fc
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-13 13:53:44 +0100

    Add files via upload

---
+++ UF5/Ex22/Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesUF5
+{
+    public static class Helper
+    {
+        public static void EvenMult2(int num)
+        {
+            const string MsgOdd = "El número és senar";
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num * 2);
+            } else
+            {
+                Console.WriteLine(MsgOdd);
+            }
+        }
+    }
+}
---
+++ UF5/Ex22/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex19
+    {
+        public delegate void MyOperations(int num);
+        public static void Main()
+        {
+            int num = 5;
+            MyOperations MyOperation = Helper.EvenMult2;
+            MyOperation(num);
+        }
+    }
+}
