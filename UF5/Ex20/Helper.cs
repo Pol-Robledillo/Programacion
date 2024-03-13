cc1dc864c806f910d5485acb23c82f9d7add3472
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-13 13:45:10 +0100

    Add files via upload

---
+++ UF5/Ex20/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesUF5
+{
+    public static class Helper
+    {
+        public static double CalcRaiz(int num)
+        {
+            return Math.Sqrt(num);
+        }
+    }
+}
---
+++ UF5/Ex20/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex19
+    {
+        public delegate double Raiz(int num);
+        public static void Main()
+        {
+            int num = 9;
+            Raiz CalcRaiz = Helper.CalcRaiz;
+            double result = CalcRaiz(num);
+            Console.WriteLine(result);
+        }
+    }
+}
