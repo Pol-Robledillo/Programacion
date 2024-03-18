2aa4c3e23f50af6a9f97d8d835116e99d8cb05a3
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-18 11:49:22 +0100

    Add files via upload

---
+++ UF5/Ex23/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesUF5
+{
+    public static class Helper
+    {
+        public static void GetPositionFibonacci(int num)
+        {
+            int current = 1, next = 1, aux = 0, numPosition = 0;
+            for (int i = 0; i < num; i++)
+            {
+                aux = current;
+                current = next;
+                next = aux + current;
+                if (i == num - 2)
+                {
+                    numPosition = current;
+                }
+            }
+            Console.WriteLine($"The number in the position {num} of the Fibonacci sequence is {numPosition}");
+        }
+    }
+}
---
+++ UF5/Ex23/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex23
+    {
+        public delegate void MyOperations(int num);
+        public static void Main()
+        {
+            int num = 8;
+            MyOperations MyOperation = Helper.GetPositionFibonacci;
+            MyOperation(num);
+        }
+    }
+}
