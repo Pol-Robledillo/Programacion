66ff69067fd8111064b280a05887525ba9eb94a7
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-18 12:13:05 +0100

    Add files via upload

---
+++ UF5/Ex24/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesUF5
+{
+    public static class Helper
+    {
+        public static bool IsNatural(int num)
+        {
+            return num > 0;
+        }
+    }
+}
---
+++ UF5/Ex24/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex24
+    {
+        public delegate bool MyOperations(int num);
+        public static void Main()
+        {
+            int num;
+            try
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                MyOperations MyOperation = Helper.IsNatural;
+                Console.WriteLine(MyOperation(num) ? "És natural" : "No és natural");
+            } catch (Exception OverflowException)
+            {
+                Console.WriteLine("No és natural");
+            }
+        }
+    }
+}
