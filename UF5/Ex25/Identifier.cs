288edfb7b9b387c37c6472e9cc8ff7f15ff92fda
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-19 12:21:15 +0100

    Add files via upload

---
+++ UF5/Ex25/Identifier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ActividadesUF5
+{
+    public static class Identifier
+    {
+        public static string Clean(string id)
+        {
+            string result = id.Replace(" ", "_");
+
+            Regex pattern = new Regex(@"[\x00-\x1F\x7F]");
+            result = pattern.Replace(result, "CTRL");
+            if (result != null && result.Contains("-"))
+            {
+                string[] words = result.Split('-');
+                result = words[0];
+                for (int i = 0; i < words.Length; i++)
+                {
+                    result += words[i].Substring(0, 1).ToUpper() + words[i].Substring(1);
+                }
+            }
+
+            Regex pattern2 = new Regex(@"[^a-zA-Z_ΟàḃḂçĐ]");
+            result = pattern2.Replace(result, "");
+
+            return result;
+        }
+    }
+}
---
+++ UF5/Ex25/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex25
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Ex1");
+            Console.WriteLine(Identifier.Clean("my   Id"));
+            Console.WriteLine();
+            Console.WriteLine("Ex2");
+            Console.WriteLine(Identifier.Clean("my\0Id"));
+            Console.WriteLine();
+            Console.WriteLine("Ex3");
+            Console.WriteLine(Identifier.Clean("a-bc"));
+            Console.WriteLine();
+            Console.WriteLine("Ex4");
+            Console.WriteLine(Identifier.Clean("1😀2😀3😀"));
+            Console.WriteLine();
+            Console.WriteLine("Ex5");
+            Console.WriteLine(Identifier.Clean("MyΟβιεγτFinder"));
+        }
+    }
+}
