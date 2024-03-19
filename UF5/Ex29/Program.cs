e009b1416586af59ef19ab4660d5d16d76b937cc
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-19 12:46:30 +0100

    Add files via upload

---
+++ UF5/Ex29/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex29
+    {
+        public static void Main()
+        {
+            List<int> lista = [ -1, 2, -3, 2, 5, 6, -7, 8, -1, -10 ];
+            var linqQuery = from num in lista
+                            group num by num into counter
+                            select new { Number = counter.Key, Frequency = counter.Count()};
+            foreach (var num in linqQuery)
+            {
+                Console.WriteLine(num.Number + ": " + num.Frequency);
+            }
+        }
+    }
+}
