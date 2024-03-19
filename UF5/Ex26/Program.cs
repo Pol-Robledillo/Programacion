8a08fe6f6997419fdc7c39f3fefdd1a82b0c3835
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-19 12:26:06 +0100

    Add files via upload

---
+++ UF5/Ex26/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex26
+    {
+        public static void Main()
+        {
+            List<int> lista = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+            var linqQuery = from num in lista
+                            where num % 2 == 0
+                            select num;
+            foreach (int num in linqQuery)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
