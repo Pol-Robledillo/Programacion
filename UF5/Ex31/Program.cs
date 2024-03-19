6d78937261b7d8a5610a52193a2a508bfc88f327
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-19 12:53:59 +0100

    Add files via upload

---
+++ UF5/Ex31/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex31
+    {
+        public static void Main()
+        {
+            List<int> days = new List<int> { 0, 1, 2, 3, 4, 5, 6 };
+            var linqQuery = from day in days
+                            select (DayOfWeek)day;
+            foreach (var day in linqQuery)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
