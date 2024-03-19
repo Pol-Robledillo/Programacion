10d96f48edd2fbff52d23cf2d4619f2908f35b47
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-19 13:15:06 +0100

    Add files via upload

---
+++ UF5/Ex34/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex34
+    {
+        public static void Main()
+        {
+            const string InputMsg = "Introduce 5 numeros: ";
+            const string OutputMsg = "Numeros mayores de 80: ";
+            List<int> nums = new List<int>();
+            Console.WriteLine(InputMsg);
+            for (int i = 0; i < 5; i++)
+            {
+                nums.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            var linqQuery = from num in nums
+                            where num > 80
+                            select num;
+            Console.WriteLine(OutputMsg);
+            foreach (var num in linqQuery)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
