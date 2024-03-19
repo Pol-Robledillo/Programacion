2a880e9582a4d14f305a2b21c9628f9ba0792565
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-19 13:05:24 +0100

    Add files via upload

---
+++ UF5/Ex32/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex32
+    {
+        public static void Main()
+        {
+            List<int> nums = new List<int> { 3, 2, 2, 3, 4, 2, 6 };
+            var linqQuery = from num in nums
+                            group num by num into g
+                            select new { Number = g.Key, Count = g.Count(), Mult = g.Key * g.Count() };
+            foreach (var num in linqQuery)
+            {
+                Console.WriteLine(num.Number + " * " + num.Count + " = " + num.Mult);
+            }
+        }
+    }
+}
