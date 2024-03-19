0c1bc195725527bfa2d74ce9017bd110f0dc9edf
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-19 12:49:34 +0100

    Add files via upload

---
+++ UF5/Ex30/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex30
+    {
+        public static void Main()
+        {
+            string word = "Robledillo";
+            var linqQuery = from c in word
+                            group c by c into counter
+                            select new { Character = counter.Key, Frequency = counter.Count()};
+            foreach (var c in linqQuery)
+            {
+                Console.WriteLine(c.Character + ": " + c.Frequency);
+            }
+        }
+    }
+}
