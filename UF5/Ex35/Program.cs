81ef1a4d0692d810a6def2da431658b52cfaa090
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-19 13:23:35 +0100

    Add files via upload

---
+++ UF5/Ex35/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex35
+    {
+        public static void Main()
+        {
+            string text = "HOLA que Tal";
+            string[] words = text.Split();
+            var linqQuery = from word in words
+                            where word == word.ToUpper()
+                            select word;
+            foreach (var word in linqQuery)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
