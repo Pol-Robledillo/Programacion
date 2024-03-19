12d839e103778b66ebb7bc39190d84c41a9c7674
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-03-19 13:10:25 +0100

    Add files via upload

---
+++ UF5/Ex33/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex33
+    {
+        public static void Main()
+        {
+            List<string> cities = new List<string> { "ROMA", "LONDRES", "NAIROBI", "CALIFÒRNIA", "ZURICH", "NOVA DELHI", "AMSTERDAM", "ABU DHABI", "PARÍS" };
+            var linqQuery = (string start, string end) => from city in cities
+                                                          where city.StartsWith(start) && city.EndsWith(end)
+                                                          select city;
+            string first = Console.ReadLine().ToUpper();
+            string last = Console.ReadLine().ToUpper();
+            foreach (var city in linqQuery(first, last))
+            {
+                Console.WriteLine(city);
+            }
+        }
+    }
+}
