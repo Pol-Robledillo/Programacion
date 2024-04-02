3b1c5b73a2dda020900502b6f4b358396f9b744f
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-04-02 11:44:35 +0200

    Add files via upload

---
+++ UF5/Ex36/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ActividadesUF5
+{
+    public class Ex36
+    {
+        public static void Main()
+        {
+            List<string> strings = [ "cat", "dog", "rat" ];
+
+            string result = strings.Aggregate((x, y) => x + y);
+            Console.WriteLine(result);
+        }
+    }
+}
